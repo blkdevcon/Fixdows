3659d83dd4a7ec244d0d0f2e6f8e020e41dd2173
Oxygemo <alej0hio2007@gmail.com>
2021-03-07 13:29:48 +0100

    Release 1.1

--- Fixdows/MainWindow.xaml.cs
+++ Fixdows/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 using System.Text;
 using System.Windows;
@@ -17,6 +21,7 @@ namespace Fixdows
         {
             InitializeComponent();
             var main = this;
+            File.Delete("relinstaller.exe");
         }
 
         private void AboutRedirButtonGithub_Click(object sender, RoutedEventArgs e)
@@ -26,7 +31,7 @@ namespace Fixdows
 
         private void AboutRedirButtonDiscord_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("https://discord.gg/u3MEEaMq2Y");
+            Process.Start("https://discord.gg/HYrafUqq73");
         }
 
         private void AboutRedirButtonMyEmail_Click(object sender, RoutedEventArgs e)
@@ -92,12 +97,50 @@ namespace Fixdows
             ws.Verb = "runas"; // Just to make sure that we launch as administrator
             Process.Start(ws); // Now we run the integrity check script
         }
+
         private void UpdateCheckButtonClicked(object sender, RoutedEventArgs e)
         {
             UpdateUI updui = new UpdateUI();
             updui.Show();
-            var version = "devel";
+            var version = "1.1"; // Version we'll use to compare & to show on the update UI
+            string update_url = "https://api.github.com/repos/odyssey346/fixdows/releases/latest"; // Our latest github release. Please don't change this.
             updui.updateui_this.Status = "Current version: " + version;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(update_url);
+            request.UserAgent = "Fixdows-Update-Tool"; // I'd prefer to not have to put in the useragent but it returns a 403 if I don't.
+            request.Method = "GET"; // We're not POSTing.
+            request.Accept = "application/vnd.github.v3+json"; // Just in case
+            HttpWebResponse GhReleaseRes = (HttpWebResponse)request.GetResponse();
+            updui.updateui_this.StatusLabelUpdate = "Sent request to GitHub to get release data, got this in response: " + GhReleaseRes.StatusCode; // This shows the user if the request went through
+            Stream dataStream = GhReleaseRes.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string strResponse = reader.ReadToEnd();
+            dynamic data = JObject.Parse(strResponse);
+            Console.WriteLine(strResponse);
+            Console.WriteLine(data.tag_name);
+            updui.UPSTREAMVERSION_LABEL.Content = "Current GitHub version: " + data.tag_name;
+            string updatetagname_str = Convert.ToString(data.tag_name);
+            Console.WriteLine(updatetagname_str);
+            if (version.Equals(updatetagname_str))
+            {
+                Console.WriteLine("we're up to date");
+                updui.updateui_this.StatusLabelUpdate = "You're up to date.";
+
+            }
+            else
+            {
+                Console.WriteLine("we're outdated!");
+                var dir = Directory.GetCurrentDirectory();
+                updui.updateui_this.StatusLabelUpdate = "Downloading release version " + updatetagname_str;
+                string updatereleasezip = "https://github.com/Odyssey346/Fixdows/releases/latest/download/Fixdows-" + data.tag_name +"-installer.exe";
+                WebClient webClient = new WebClient();
+                Console.WriteLine(updatereleasezip);
+                webClient.DownloadFile(updatereleasezip, @dir + "\\relinstaller.exe");
+                string zipPath = @dir + "\\relinstaller.exe";
+                var installer = new ProcessStartInfo(zipPath);
+                installer.Verb = "runas"; // Just to make sure that we launch as administrator
+                installer.Arguments = "/DIR=" + dir + "  /LOG /CLOSEAPPLICATIONS"; // Arguments for the installer program
+                Process.Start(installer); // Now we run the installer. 
+            }   
         }
     }
 }
--- Fixdows/UpdateUI.xaml.cs
+++ Fixdows/UpdateUI.xaml.cs
@@ -31,5 +31,17 @@ namespace Fixdows
             get { return CurrentVersion_UpdUI.Content.ToString(); }
             set { Dispatcher.Invoke(new Action(() => { CurrentVersion_UpdUI.Content = value; })); }
         }
+
+        internal string StatusLabelUpdate
+        {
+            get { return CurrentVersion_UpdUI.Content.ToString(); }
+            set { Dispatcher.Invoke(new Action(() => { UPDATE_STATUS_LABEL.Content = value; })); }
+        }
+
+        internal string UpstreamLabelUpdate
+        {
+            get { return CurrentVersion_UpdUI.Content.ToString(); }
+            set { Dispatcher.Invoke(new Action(() => { UPSTREAMVERSION_LABEL.Content = value; })); }
+        }
     }
 }
